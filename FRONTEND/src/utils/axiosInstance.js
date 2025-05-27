a3bf9117397aac9da97fdc03e0cb87abf2d3db61
masterShashi <shashikumargupta443@gmail.com>
2025-05-27 23:53:00 +0530

    fixed some cors error

--- FRONTEND/src/utils/axiosInstance.js
+++ FRONTEND/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://url-shortener-rk77.onrender.com",
+    baseURL: "http://localhost:3000",
     headers: {
         "Content-Type": "application/json",
     },
