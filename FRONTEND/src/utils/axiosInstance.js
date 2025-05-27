ff7b9101248f6ff3b5149b3b58608bfaa54c2da6
masterShashi <shashikumargupta443@gmail.com>
2025-05-27 23:40:25 +0530

    updated base url

--- FRONTEND/src/utils/axiosInstance.js
+++ FRONTEND/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://url-shortener-rk77.onrender.com",
     headers: {
         "Content-Type": "application/json",
     },
